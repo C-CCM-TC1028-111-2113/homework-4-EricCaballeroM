3a8431db938ada1c9e34f8bd6c1741ff1cb15594
EricCaballeroM <88851026+EricCaballeroM@users.noreply.github.com>
2021-09-05 19:27:52 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+    #Alterna Caracteres
+
+n=int(input("dame un número: "))
+a=0
+
+while a<n:
+   a+=1
+   print(str(a)+"-#")
+   if a<n:
+       a+=1
+       print(str(a)+"-%")
     pass
 
 if __name__=='__main__':   
