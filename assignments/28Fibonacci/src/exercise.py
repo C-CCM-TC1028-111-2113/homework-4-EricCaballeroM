28a8eeb7eb42a8a6b4d3fd8daaa33653e9f6a920
EricCaballeroM <88851026+EricCaballeroM@users.noreply.github.com>
2021-09-06 22:43:55 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,6 +1,21 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    #Números de Fibonacci
+def numeros(): 
+    num=int(input('dame un numero entero positivo: '))
+
+    a=0
+    b=1
+
+    for x in range(num-1):
+        fib=a+b
+        a=b
+        b=fib
+    return b
+
+main=numeros()
+print(main)
     pass
 
 if __name__=='__main__':
